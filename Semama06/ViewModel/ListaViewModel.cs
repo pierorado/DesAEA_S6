5ebe51e13d1064d61d948c99797efd0bf0f66ed7
Piero <delpiee@hotmail.com>
2020-05-04 22:49:18 -0500

    Agregar archivos de proyecto.

---
+++ Semama06/ViewModel/ListaViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Semama06.ViewModel
+{
+    public class ListaCategoriaViewModel : ViewModelBase
+    {
+        public ObservableCollection<Categoria> Categorias { get; set; }
+        public ICommand NuevoComand { set; get; }
+        public ICommand ConsultarCommand { set; get; }
+        public ListaCategoriaViewModel()
+        {
+            Categorias = new Model.CategoriaModel().Categorias;
+            NuevoComand = new RelayCommand<Window>(
+                param => Abrir()
+                );
+            ConsultarCommand = new RelayCommand<object>(o => { Categorias = (new Model.CategoriaModel()).Categorias; });
+            void Abrir()
+            {
+                View.ManCategoria window = new View.ManCategoria();
+                window.ShowDialog();
+                o=> { Categoria = (new Model.CategoriaModel()).Categorias; };
+            }
+            void Abrir()
+            {
+                View.ManCategoria window = new View.ManCategoria();
+                window.ShowDialog()
+            }
+        }
+    }
+}
